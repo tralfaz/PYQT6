9e8f66b3443f12930e23d6646ccc80b59473e469
Michael Marchionna <mdm@TralfazBook.attlocal.net>
2023-12-24 15:10:50 -0800

    Complete example

--- std_shortcuts.py
+++ std_shortcuts.py
@@ -1,11 +1,14 @@
 from PyQt6 import QtCore
 from PyQt6 import QtGui
 from PyQt6.QtGui import QAction
+from PyQt6.QtGui import QActionGroup
 from PyQt6.QtGui import QKeySequence
 from PyQt6.QtWidgets import QApplication
 from PyQt6.QtWidgets import QFrame
 from PyQt6.QtWidgets import QLabel
 from PyQt6.QtWidgets import QMainWindow
+from PyQt6.QtWidgets import QMenu
+from PyQt6.QtWidgets import QMessageBox
 from PyQt6.QtWidgets import QPushButton
 from PyQt6.QtWidgets import QSizePolicy
 from PyQt6.QtWidgets import QVBoxLayout
@@ -134,51 +137,54 @@ class MainWin(QMainWindow):
                                             "lines of a paragraph")
         self.setLineSpacingAct.triggered.connect(self._setLineSpacingCB)
 
-#    setParagraphSpacingAct = new QAction(tr("Set &Paragraph Spacing..."), this);
-#    setParagraphSpacingAct->setStatusTip(tr("Change the gap between paragraphs"));
-#    connect(setParagraphSpacingAct, &QAction::triggered,
-#            this, &MainWindow::setParagraphSpacing);
-#
-#    aboutAct = new QAction(tr("&About"), this);
-#    aboutAct->setStatusTip(tr("Show the application's About box"));
-#    connect(aboutAct, &QAction::triggered, this, &MainWindow::about);
-#
-#    aboutQtAct = new QAction(tr("About &Qt"), this);
-#    aboutQtAct->setStatusTip(tr("Show the Qt library's About box"));
-#    connect(aboutQtAct, &QAction::triggered, qApp, &QApplication::aboutQt);
-#    connect(aboutQtAct, &QAction::triggered, this, &MainWindow::aboutQt);
-#
-#    leftAlignAct = new QAction(tr("&Left Align"), this);
-#    leftAlignAct->setCheckable(true);
-#    leftAlignAct->setShortcut(tr("Ctrl+L"));
-#    leftAlignAct->setStatusTip(tr("Left align the selected text"));
-#    connect(leftAlignAct, &QAction::triggered, this, &MainWindow::leftAlign);
-#
-#    rightAlignAct = new QAction(tr("&Right Align"), this);
-#    rightAlignAct->setCheckable(true);
-#    rightAlignAct->setShortcut(tr("Ctrl+R"));
-#    rightAlignAct->setStatusTip(tr("Right align the selected text"));
-#    connect(rightAlignAct, &QAction::triggered, this, &MainWindow::rightAlign);
-#
-#    justifyAct = new QAction(tr("&Justify"), this);
-#    justifyAct->setCheckable(true);
-#    justifyAct->setShortcut(tr("Ctrl+J"));
-#    justifyAct->setStatusTip(tr("Justify the selected text"));
-#    connect(justifyAct, &QAction::triggered, this, &MainWindow::justify);
-#
-#    centerAct = new QAction(tr("&Center"), this);
-#    centerAct->setCheckable(true);
-#    centerAct->setShortcut(tr("Ctrl+E"));
-#    centerAct->setStatusTip(tr("Center the selected text"));
-#    connect(centerAct, &QAction::triggered, this, &MainWindow::center);
-#
-#//! [6] //! [7]
-#    alignmentGroup = new QActionGroup(this);
-#    alignmentGroup->addAction(leftAlignAct);
-#    alignmentGroup->addAction(rightAlignAct);
-#    alignmentGroup->addAction(justifyAct);
-#    alignmentGroup->addAction(centerAct);
-#    leftAlignAct->setChecked(true);
+        self.setParagraphSpacingAct = QAction("Set &Paragraph Spacing...", self)
+        self.setParagraphSpacingAct.setStatusTip("Change the gap between paragraphs")
+        self.setParagraphSpacingAct.triggered.connect(self._setParagraphSpacingCB)
+
+        self.finsterAct = QAction("Finster", self)
+#        self.finsterAct.setShortcuts(QKeySequence.StandardKey.Undo)
+        self.finsterAct.setStatusTip("The finster dummy action")
+        self.finsterAct.triggered.connect(self._aboutCB)
+
+        self.aboutAct = QAction("About", self)
+        self.aboutAct.setStatusTip("Show the application's About box")
+        self.aboutAct.triggered.connect(self._aboutCB)
+
+        self.aboutQtAct = QAction("About &Qt", self)
+        self.aboutQtAct.setStatusTip("Show the Qt library's About box")
+#        self.aboutQtAct.triggered.connect(d, qApp, &QApplication::aboutQt)
+        self.aboutQtAct.triggered.connect(self._aboutQtCB)
+
+        self.leftAlignAct = QAction("&Left Align", self)
+        self.leftAlignAct.setCheckable(True)
+        self.leftAlignAct.setShortcut("Ctrl+L")
+        self.leftAlignAct.setStatusTip("Left align the selected text")
+        self.leftAlignAct.triggered.connect(self._leftAlignCB)
+
+        self.rightAlignAct = QAction("&Right Align", self)
+        self.rightAlignAct.setCheckable(True)
+        self.rightAlignAct.setShortcut("Ctrl+R")
+        self.rightAlignAct.setStatusTip("Right align the selected text")
+        self.rightAlignAct.triggered.connect(self._rightAlignCB)
+
+        self.justifyAct = QAction("&Justify", self)
+        self.justifyAct.setCheckable(True)
+        self.justifyAct.setShortcut("Ctrl+J")
+        self.justifyAct.setStatusTip("Justify the selected text")
+        self.justifyAct.triggered.connect(self._justifyCB)
+
+        self.centerAct = QAction("&Center", self)
+        self.centerAct.setCheckable(True)
+        self.centerAct.setShortcut("Ctrl+E")
+        self.centerAct.setStatusTip("Center the selected text")
+        self.centerAct.triggered.connect(self._centerCB)
+
+        self.alignmentGroup = QActionGroup(self)
+        self.alignmentGroup.addAction(self.leftAlignAct)
+        self.alignmentGroup.addAction(self.rightAlignAct)
+        self.alignmentGroup.addAction(self.justifyAct)
+        self.alignmentGroup.addAction(self.centerAct)
+#        self.leftAlignAct.setChecked(True)
 
     def _createMenus(self):
         fileMenu = self.menuBar().addMenu("&File")
@@ -198,26 +204,33 @@ class MainWin(QMainWindow):
         editMenu.addAction(self.pasteAct)
         editMenu.addSeparator()
 
-#    helpMenu = menuBar()->addMenu(tr("&Help"));
-#    helpMenu->addAction(aboutAct);
-#    helpMenu->addAction(aboutQtAct);
+        helpMenu = self.menuBar().addMenu("&Help")
+        helpMenu.addAction(self.aboutAct)
+        helpMenu.addAction(self.undoAct)
+        helpMenu.addAction(self.finsterAct)
+        helpMenu.addAction(self.aboutQtAct)
 
         formatMenu = editMenu.addMenu("&Format")
         formatMenu.addAction(self.boldAct)
         formatMenu.addAction(self.italicAct)
         formatMenu.addSeparator().setText("Alignment")
-#    formatMenu->addAction(leftAlignAct);
-#    formatMenu->addAction(rightAlignAct);
-#    formatMenu->addAction(justifyAct);
-#    formatMenu->addAction(centerAct);
-#    formatMenu->addSeparator();
+        formatMenu.addAction(self.leftAlignAct)
+        formatMenu.addAction(self.rightAlignAct)
+        formatMenu.addAction(self.justifyAct)
+        formatMenu.addAction(self.centerAct)
+        formatMenu.addSeparator()
         formatMenu.addAction(self.setLineSpacingAct)
-#    formatMenu->addAction(setParagraphSpacingAct);
+        formatMenu.addAction(self.setParagraphSpacingAct)
 
     
     def contextMenuEventCB(self, qev):
         """   qev, QContextMenuEvent"""
         print("contextMenuEventCB")
+#    QMenu menu(this);
+#    menu.addAction(cutAct);
+#    menu.addAction(copyAct);
+#    menu.addAction(pasteAct);
+#    menu.exec(event->globalPos());
 
     def _newFileCB(self):
         print("_newFileCB")
@@ -254,22 +267,22 @@ class MainWin(QMainWindow):
         self.infoLabel.setText("Invoked <b>Edit|Format|Italic</b>")
 
     def _leftAlignCB(self):
-        self.infoLabel.setText(tr("Invoked <b>Edit|Format|Left Align</b>"));
+        self.infoLabel.setText("Invoked <b>Edit|Format|Left Align</b>")
 
     def _rightAlignCB(self):
-        self.infoLabel.setText(tr("Invoked <b>Edit|Format|Right Align</b>"));
+        self.infoLabel.setText("Invoked <b>Edit|Format|Right Align</b>")
 
     def _justifyCB(self):
-        self.infoLabel.setText(tr("Invoked <b>Edit|Format|Justify</b>"));
+        self.infoLabel.setText("Invoked <b>Edit|Format|Justify</b>")
 
     def _centerCB(self):
-        self.infoLabel.setText(tr("Invoked <b>Edit|Format|Center</b>"));
+        self.infoLabel.setText("Invoked <b>Edit|Format|Center</b>")
 
     def _setLineSpacingCB(self):
         self.infoLabel.setText("Invoked <b>Edit|Format|Set Line Spacing</b>")
 
     def _setParagraphSpacingCB(self):
-        self.infoLabel.setText(tr("Invoked <b>Edit|Format|Set Paragraph Spacing</b>"));
+        self.infoLabel.setText("Invoked <b>Edit|Format|Set Paragraph Spacing</b>")
 
     def _aboutCB(self):
         self.infoLabel.setText("Invoked <b>Help|About</b>")
